3bdd4ce421049ecc3876416356697e2dbd97438c
Clint Kunz <37884315+clintKunz@users.noreply.github.com>
2019-02-26 16:53:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,35 @@ const port = 8500;
 server.use(express.json());
 
 server.get('/project/:id', (req, res) => {
+    //query the projects table to get a specific project
     
+    //query the actions table to get all actions associated with that specific project
+    
+    //add the actions onto the project -> project.actions = actions
+    
+    //response should have the following object shape
+    //{
+    //"id": 1,
+    //"name": "sprint",
+    //"description": "complete MVP by 12",
+    //"completed": 0,
+    //"actions": [
+    //    {
+    //        "id": 1,
+    //        "description": "add post to action",
+    //        "notes": null,
+    //        "completed": 0,
+    //        "project_id": 1
+    //    },
+    //    {
+    //        "id": 2,
+    //        "description": "return project",
+    //        "notes": null,
+    //        "completed": 0,
+    //        "project_id": 1
+    //    }
+    //  ]
+    //}
 });
 
 server.listen(port, () => console.log(`\nAPI running on http://localhost:${port}\n`));
