3140f97c51f2c30e6ecd0a0119fc4d3ac2d3eb1c
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-27 12:08:08 -0500

    Last commit before practice Day 2

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ server.use(express.json());
 
 server.get('/project/:id', (req, res) => {
     //query the projects table to get a specific project
-    
+    db("projects")
+    .where({id: req.params.id})
+    .first()
     //query the actions table to get all actions associated with that specific project
+    .then(banana => {
+        if (banana) {
+            db("actions")
+            .where({project_id: id})
+            .then()
+        } else {
+            res.status(404).json({message: "no project found with that id"})
+        }
+    })
     
     //add the actions onto the project -> project.actions = actions
    
